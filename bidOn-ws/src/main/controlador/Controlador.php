1e91b5d66016901b7e4522e04f778089dfc70f27
hesparza <hiram.esparza@gmail.com>
2015-03-22 21:29:14 -0600

    Se modifico la funcion para manejo de errores ya que se encontraba deprecada en php 5.2, se agrego un manejador de errores que es lo que ahora se utiliza

--- bidOn-ws/src/main/controlador/Controlador.php
+++ bidOn-ws/src/main/controlador/Controlador.php
@@ -36,12 +36,59 @@ class Controlador {
 		$this->_parametros = $parametros;		
 		$this->procesadorDeRespuestas = new ProcesadorDeRespuestas();
 
-		set_exception_handler(function ($e) use ($_formato) {
-			http_response_code($e->getCode());
-			$this->procesadorDeRespuestas->procesarRespuesta( $this->_formato, $e->getCode(), $e->getMessage(), '');			
-		});
+// 		$lambda = 		function ($e) use ($_formato) {
+// 			http_response_code($e->getCode());
+// 			$this->procesadorDeRespuestas->procesarRespuesta( $this->_formato, $e->getCode(), $e->getMessage(), '');
+// 		}
+		
+// 		set_exception_handler($lambda);
+		
+		//Se registra una funcion para manejar errores en la aplicacion
+		register_shutdown_function( "manejador_de_errores" );
+		
+// 		$lambda = function($exception) use ($var) {
+// 			$this->callback($exception,$var);
+// 		}
+		
+// 		set_exception_handler($lambda);
 
 		$this->_negocios =  new Negocios();
+	}		
+	
+	function manejador_de_errores() {
+		$errfile = "unknown file";
+		$errstr  = "shutdown";
+		$errno   = E_CORE_ERROR;
+		$errline = 0;
+	
+		$error = error_get_last();
+	
+		if( $error !== NULL) {
+			$errno   = $error["type"];
+			$errfile = $error["file"];
+			$errline = $error["line"];
+			$errstr  = $error["message"];
+	
+			format_error( $errno, $errstr, $errfile, $errline);
+		}
+	}
+	
+	function format_error( $errno, $errstr, $errfile, $errline ) {
+		$trace = print_r( debug_backtrace( false ), true );
+	
+		$content  = "<table><thead bgcolor='#c8c8c8'><th>Item</th><th>Description</th></thead><tbody>";
+		$content .= "<tr valign='top'><td><b>Error</b></td><td><pre>$errstr</pre></td></tr>";
+		$content .= "<tr valign='top'><td><b>Errno</b></td><td><pre>$errno</pre></td></tr>";
+		$content .= "<tr valign='top'><td><b>File</b></td><td>$errfile</td></tr>";
+		$content .= "<tr valign='top'><td><b>Line</b></td><td>$errline</td></tr>";
+		$content .= "<tr valign='top'><td><b>Trace</b></td><td><pre>$trace</pre></td></tr>";
+		$content .= '</tbody></table>';
+	
+		return $content;
+	}
+		
+	public static function callback($exception, $var) {
+		// process $exception using $var
 	}
 
 	/**
